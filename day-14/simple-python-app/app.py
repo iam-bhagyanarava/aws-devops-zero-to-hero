efa53d2e023b0f3e3e2a8913b9ad0d65d76863e4
iam-bhagyanarava <bhagyanarava1999@gmail.com>
2026-01-29 17:35:21 +0530

    Add comments to app.py for clarity
    Added comments explaining Flask app initialization and __name__ variable.

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -1,5 +1,12 @@
-from flask import Flask
+#Flask is a class provided by the Flask library.
+#When you call Flask(__name__), you are creating an instance of the Flask app. That instance is typically called app.
+__name__ is a Python built-in variable.
+#If your script is the main program being run, __name__ will be "__main__".
+#If your script is imported as a module in another script, __name__ will be the module name.
+#3️⃣ Why pass __name__ to Flask?
+#Flask uses this argument to know where your application is located. 
 
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,4 +15,3 @@ def hello():
 
 if __name__ == '__main__':
     app.run()
-
